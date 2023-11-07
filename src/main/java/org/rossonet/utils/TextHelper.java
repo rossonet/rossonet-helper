40331a91a5ebd84afbcffae635f46dfdca7b6228
Andrea Ambrosini <andrea.ambrosini@rossonet.org>
2023-11-07 15:40:44 +0100

    static for ANSI terminal

--- src/main/java/org/rossonet/utils/TextHelper.java
+++ src/main/java/org/rossonet/utils/TextHelper.java
@@ -25,7 +25,80 @@ import org.json.JSONObject;
 
 public final class TextHelper {
 
-	private static final String ENCRYPTION_ALGORITHM = "AES";
+	private static String encryptionAlgorithm = "AES";
+
+	// Reset
+	public static final String ANSI_RESET = "\033[0m"; // Text Reset
+
+	// Regular Colors
+	public static final String ANSI_BLACK = "\033[0;30m"; // BLACK
+	public static final String ANSI_RED = "\033[0;31m"; // RED
+	public static final String ANSI_GREEN = "\033[0;32m"; // GREEN
+	public static final String ANSI_YELLOW = "\033[0;33m"; // YELLOW
+	public static final String ANSI_BLUE = "\033[0;34m"; // BLUE
+	public static final String ANSI_PURPLE = "\033[0;35m"; // PURPLE
+	public static final String ANSI_CYAN = "\033[0;36m"; // CYAN
+	public static final String ANSI_WHITE = "\033[0;37m"; // WHITE
+
+	// Bold
+	public static final String ANSI_BLACK_BOLD = "\033[1;30m"; // BLACK
+	public static final String ANSI_RED_BOLD = "\033[1;31m"; // RED
+	public static final String ANSI_GREEN_BOLD = "\033[1;32m"; // GREEN
+	public static final String ANSI_YELLOW_BOLD = "\033[1;33m"; // YELLOW
+	public static final String ANSI_BLUE_BOLD = "\033[1;34m"; // BLUE
+	public static final String ANSI_PURPLE_BOLD = "\033[1;35m"; // PURPLE
+	public static final String ANSI_CYAN_BOLD = "\033[1;36m"; // CYAN
+	public static final String ANSI_WHITE_BOLD = "\033[1;37m"; // WHITE
+
+	// Underline
+	public static final String ANSI_BLACK_UNDERLINED = "\033[4;30m"; // BLACK
+	public static final String ANSI_RED_UNDERLINED = "\033[4;31m"; // RED
+	public static final String ANSI_GREEN_UNDERLINED = "\033[4;32m"; // GREEN
+	public static final String ANSI_YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+	public static final String ANSI_BLUE_UNDERLINED = "\033[4;34m"; // BLUE
+	public static final String ANSI_PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
+	public static final String ANSI_CYAN_UNDERLINED = "\033[4;36m"; // CYAN
+	public static final String ANSI_WHITE_UNDERLINED = "\033[4;37m"; // WHITE
+
+	// Background
+	public static final String ANSI_BLACK_BACKGROUND = "\033[40m"; // BLACK
+	public static final String ANSI_RED_BACKGROUND = "\033[41m"; // RED
+	public static final String ANSI_GREEN_BACKGROUND = "\033[42m"; // GREEN
+	public static final String ANSI_YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+	public static final String ANSI_BLUE_BACKGROUND = "\033[44m"; // BLUE
+	public static final String ANSI_PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+	public static final String ANSI_CYAN_BACKGROUND = "\033[46m"; // CYAN
+	public static final String ANSI_WHITE_BACKGROUND = "\033[47m"; // WHITE
+
+	// High Intensity
+	public static final String ANSI_BLACK_BRIGHT = "\033[0;90m"; // BLACK
+	public static final String ANSI_RED_BRIGHT = "\033[0;91m"; // RED
+	public static final String ANSI_GREEN_BRIGHT = "\033[0;92m"; // GREEN
+	public static final String ANSI_YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+	public static final String ANSI_BLUE_BRIGHT = "\033[0;94m"; // BLUE
+	public static final String ANSI_PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+	public static final String ANSI_CYAN_BRIGHT = "\033[0;96m"; // CYAN
+	public static final String ANSI_WHITE_BRIGHT = "\033[0;97m"; // WHITE
+
+	// Bold High Intensity
+	public static final String ANSI_BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
+	public static final String ANSI_RED_BOLD_BRIGHT = "\033[1;91m"; // RED
+	public static final String ANSI_GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
+	public static final String ANSI_YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
+	public static final String ANSI_BLUE_BOLD_BRIGHT = "\033[1;94m"; // BLUE
+	public static final String ANSI_PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
+	public static final String ANSI_CYAN_BOLD_BRIGHT = "\033[1;96m"; // CYAN
+	public static final String ANSI_WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
+
+	// High Intensity backgrounds
+	public static final String ANSI_BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
+	public static final String ANSI_RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
+	public static final String ANSI_GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+	public static final String ANSI_YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
+	public static final String ANSI_BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
+	public static final String ANSI_PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
+	public static final String ANSI_CYAN_BACKGROUND_BRIGHT = "\033[0;106m"; // CYAN
+	public static final String ANSI_WHITE_BACKGROUND_BRIGHT = "\033[0;107m"; // WHITE
 
 	public static String convertByteArrayToHexString(final byte[] arrayBytes) {
 		final StringBuffer stringBuffer = new StringBuffer();
@@ -37,7 +110,7 @@ public final class TextHelper {
 
 	public static byte[] decryptData(final byte[] encryptedData, final byte[] key) throws InvalidKeyException,
 			NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-		return decryptData(encryptedData, key, ENCRYPTION_ALGORITHM);
+		return decryptData(encryptedData, key, encryptionAlgorithm);
 	}
 
 	public static byte[] decryptData(final byte[] encryptedData, final byte[] key, String encryptionAlgorithm)
@@ -51,7 +124,7 @@ public final class TextHelper {
 
 	public static byte[] encryptData(final byte[] dataToEncrypt, final byte[] key) throws IllegalBlockSizeException,
 			BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
-		return encryptData(dataToEncrypt, key, ENCRYPTION_ALGORITHM);
+		return encryptData(dataToEncrypt, key, encryptionAlgorithm);
 	}
 
 	public static byte[] encryptData(final byte[] dataToEncrypt, final byte[] key, String encryptionAlgorithm)
@@ -61,6 +134,10 @@ public final class TextHelper {
 		final SecretKeySpec k = new SecretKeySpec(key, encryptionAlgorithm);
 		c.init(Cipher.ENCRYPT_MODE, k);
 		return c.doFinal(dataToEncrypt);
+	}
+
+	public static String getDefaultEncryptionAlgorithm() {
+		return encryptionAlgorithm;
 	}
 
 	public static JSONObject getJsonFromMap(final Map<String, String> map) {
@@ -125,6 +202,10 @@ public final class TextHelper {
 		oos.writeObject(object);
 		oos.close();
 		return Base64.getEncoder().encodeToString(baos.toByteArray());
+	}
+
+	public static void setDefaultEncryptionAlgorithm(String encryptionAlgorithm) {
+		TextHelper.encryptionAlgorithm = encryptionAlgorithm;
 	}
 
 	public static List<String> splitFixSize(final String s, final int chunkSize) {
